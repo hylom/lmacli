62d744507368b13b70d52a9ad9f9400f1cb46767
Hiromichi Matsushima <hylom@hylom.net>
2020-07-28 17:44:59 +0900

    add audience manage functions

---
+++ lib/audience.py
@@ -0,0 +1,47 @@
+"""LINE Message API: audience managing related API"""
+
+from linebot.models import TextSendMessage
+import urllib
+import json
+
+from .interface import Interface
+
+class AudienceInterface(Interface):
+    def create_upload(self,
+                      ids,
+                      description,
+                      upload_description="",
+                      is_ifa_audience=False):
+        audiences = [ dict(id=x) for x in ids ]
+        data = {
+            'description': description,
+            'isIfaAudience': is_ifa_audience,
+            'uploadDescription': upload_description,
+            'audiences': audiences,
+        }
+        response = self.get_api_client()._post(
+            '/v2/bot/audienceGroup/upload',
+            data=json.dumps(data, sort_keys=True),
+            headers={'Content-Type': 'application/json'},
+            timeout=timeout
+        )
+        return response.json.get('audienceGroupId')
+
+    def list(self,
+             page=1,
+             description="",
+             status="",
+             size=20,
+             includes_external_public_groups="",
+             create_route=""):
+        data = {
+            "page": page,
+            "description": description,
+            "status": status,
+            "size": size,
+            "includeExternalpublicGroups": includes_external_public_groups,
+            "createRoute": create_route,
+        }
+        url = '/v2/bot/audienceGroup/list?' + urllib.parse.urlencode(data)
+        response = self.get_api_client()._get(url)
+        return response.json
